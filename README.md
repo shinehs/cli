1eaea090f47e16461eb4c017557843add2924947
huangshuai <nutsuf@126.com>
2017-05-24 16:58:32 +0800

    优化目录结构

--- README.md
+++ README.md
@@ -1,13 +1,8 @@
 tool本地工具压缩工具。
+需要压缩的文件放在js/css这个文件夹，压缩好的文件会在output_js/output_css下
 
-introduce
-1.目标文件js(css)，输出output_js(output_css)。
-2.全局使用首先请在本地目录执行(npm link),若和本地命令有冲突请重命名：package.json->bin->tool
-
-use:
+命令
 ============================
 tool mixin -js   ------压缩js
 
 tool mixin -css
-
-未完待续。。。
--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 var config = {
 	output:{
-		'js': {
-        path:'D:/svn/my_cli/output_js'
-    },
-    'css': {
-        path:'D:/svn/my_cli/output_css'
-    }
+      	'js': {
+            path:'D:/svn/my_cli/output_js'
+        },
+        'css': {
+            path:'D:/svn/my_cli/output_css'
+        }
 	},
 	input:{
 		'js': {
-        path:'D:/svn/my_cli/js'
-    },
-    'css': {
-        path:'D:/svn/my_cli/css'
-    }
+            path:'D:/svn/my_cli/js'
+        },
+        'css': {
+            path:'D:/svn/my_cli/css'
+        }
 	}
 };
 
