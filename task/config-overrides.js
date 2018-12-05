d784dadfb785214c0812d4ccd99bca0d00341339
shine <nutsuf@126.com>
2018-12-05 11:17:12 +0800

    新增配置覆盖文件

---
+++ task/config-overrides.js
@@ -0,0 +1,18 @@
+let path = require('path');
+
+module.exports = function override(config, env) {
+  //do stuff with the webpack config...
+  // reset
+  // config.output.publicPath = './';
+  config.output.path = path.resolve(path.resolve(__dirname, '') + '/build/yyRap/pc');
+  config.output.filename = 'js/bundle.js';
+  config.output.chunkFilename = 'js/[name].[chunkhash].chunk.js';
+  config.plugins[1].options.filename = 'html/index.html';
+  config.plugins[4].filename = 'css/[name].[contenthash:8].css';
+  // mainfest
+  config.plugins[5].opts.fileName = 'asset/asset-mainfest.json';
+  // img
+  config.module.rules[1].oneOf[0].options.name = 'images/[name].[hash:8].[ext]';
+  // console.log(config)
+  return config;
+}
